0a0c8075374824e56c2367743ae5c282773e1281
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-29 10:27:38 +0330

    Add files via upload

---
+++ ConsoleApp116/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp116
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int count = 1; count <= 10; ++count)
+            {
+                if (count == 5)
+                {
+                    continue;
+                }
+
+                Console.Write($"{count}");
+            }
+            
+            Console.WriteLine($"\n Used continue to skip");
+        }
+    }
+}
